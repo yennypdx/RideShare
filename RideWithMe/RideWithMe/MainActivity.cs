f400da814ec02385959bea34458bcf0cda23d060
ypurwantopdx <yenny.purwanto@oit.edu>
2018-11-16 03:12:43 -0800

    Added MainActivity and RegisterActivity

--- RideWithMe/RideWithMe/MainActivity.cs
+++ RideWithMe/RideWithMe/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -10,9 +11,37 @@ namespace RideWithMe
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            //REGISTER ME button
+            Button btnRegister = FindViewById<Button>(Resource.Id.btn_reg);
+            btnRegister.Click += RegisterButton_Click;
+            //RIDER button
+
+            //DRIVER button
+
+            //ADMIN button
+
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.Register);
+        }
+
+        private void RiderButton_Click(object sender, EventArgs e)
+        {
+            //SetContentView(Resource.Layout.RiderPage);
+        }
+
+        private void DriverButton_Click(object sender, EventArgs e)
+        {
+            //SetContentView(Resource.Layout.DriverPage);
+        }
+
+        private void AdminButton_Click(object sender, EventArgs e)
+        {
+            //SetContentView(Resource.Layout.AdminPage);
         }
     }
 }
--- RideWithMe/RideWithMe/Resources/Resource.Designer.cs
+++ RideWithMe/RideWithMe/Resources/Resource.Designer.cs
@@ -41,11 +41,121 @@ namespace RideWithMe
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int rmwicon = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050007
+			public const int btn_admin = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int btn_driver = 2131034118;
+			
+			// aapt resource value: 0x7f050009
+			public const int btn_reg = 2131034121;
+			
+			// aapt resource value: 0x7f050005
+			public const int btn_rider = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int input_email = 2131034113;
+			
+			// aapt resource value: 0x7f050012
+			public const int input_emailreg = 2131034130;
+			
+			// aapt resource value: 0x7f05000c
+			public const int input_fname = 2131034124;
+			
+			// aapt resource value: 0x7f05000e
+			public const int input_lname = 2131034126;
+			
+			// aapt resource value: 0x7f050014
+			public const int input_passreg = 2131034132;
+			
+			// aapt resource value: 0x7f050003
+			public const int input_passwd = 2131034115;
+			
+			// aapt resource value: 0x7f050010
+			public const int input_phone = 2131034128;
+			
+			// aapt resource value: 0x7f050018
+			public const int input_plate = 2131034136;
+			
+			// aapt resource value: 0x7f050016
+			public const int input_vtype = 2131034134;
+			
+			// aapt resource value: 0x7f050000
+			public const int label_email = 2131034112;
+			
+			// aapt resource value: 0x7f050011
+			public const int label_emailreg = 2131034129;
+			
+			// aapt resource value: 0x7f05000b
+			public const int label_fname = 2131034123;
+			
+			// aapt resource value: 0x7f05000d
+			public const int label_lname = 2131034125;
+			
+			// aapt resource value: 0x7f050004
+			public const int label_login = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int label_notreg = 2131034120;
+			
+			// aapt resource value: 0x7f050013
+			public const int label_passreg = 2131034131;
+			
+			// aapt resource value: 0x7f050002
+			public const int label_passwd = 2131034114;
+			
+			// aapt resource value: 0x7f05000f
+			public const int label_phone = 2131034127;
+			
+			// aapt resource value: 0x7f050017
+			public const int label_plate = 2131034135;
+			
+			// aapt resource value: 0x7f05000a
+			public const int label_userreg = 2131034122;
+			
+			// aapt resource value: 0x7f050015
+			public const int label_vtype = 2131034133;
+			
+			// aapt resource value: 0x7f050019
+			public const int textView9 = 2131034137;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Register = 2130903041;
 			
 			static Layout()
 			{
@@ -60,8 +170,8 @@ namespace RideWithMe
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
